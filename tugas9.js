2a7823055996d555165ef348ed1c3b8ee01d76e7
Yehezkiel Reynaldi Abraham <yehezkielreynaldi01@gmail.com>
2023-04-27 16:20:22 +0700

    Tugas 9 JS
    Tugas ini saya kirimkan untuk Niomic

---
+++ tugas9.js
@@ -0,0 +1,13 @@
+function realMadrid() {
+    let pemain = {
+        no7 : "Cristiano Ronaldo",
+        no1 : "Iker Casillas",
+        no10: "Mesut Ozil",
+        no4: "Sergio Ramos"
+    }
+    for(let x in pemain){
+        console.log(pemain[x]);
+    }
+}
+
+realMadrid();
